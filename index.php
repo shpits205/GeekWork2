5b5d527797773932a27a7738fcad00183ddca3e2
shpits205 <nykolay645>
2013-10-23 20:16:01 +0300

    index

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: shpits
+ * Date: 23.10.13
+ * Time: 20:12
+ * To change this template use File | Settings | File Templates.
+ */
+echo 'shpits';
+phpinfo();
