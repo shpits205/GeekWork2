6084647a45defb37e4ae206055b37ed5ecd0d4be
shpits205 <nykolay645>
2013-10-23 22:09:55 +0300

    Начинаємо робити домашнє завдання!

--- index.php
+++ index.php
@@ -1,10 +1,41 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: shpits
- * Date: 23.10.13
- * Time: 20:12
- * To change this template use File | Settings | File Templates.
- */
-echo 'shpits';
-phpinfo();
+abstract class Animal
+{
+    private $name;
+    private $weight;
+    private $age;
+    abstract protected  function test();
+    public   function setName($name)
+    {
+        $this->name = $name;
+    }
+    public  function setWeight($weight)
+    {
+        $this->weight = $weight;
+    }
+    public  function setAge($age)
+    {
+        $this->age = $age;
+    }
+    public   function getName()
+    {
+        return $this->name;
+    }
+    public  function getWeight()
+    {
+        return $this->weight;
+    }
+    public  function getAge()
+    {
+        return $this->age;
+    }
+}
+
+class Ptiz extends Animal
+{
+    public function test()
+    {
+        $this->setName("ShpitS");
+        $this->setWeight(222);
+    }
+}
